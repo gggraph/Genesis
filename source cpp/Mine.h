33c9d42f2c083c8024dd07086b9c358164fecd3d
gggraph <62741099+gggraph@users.noreply.github.com>
2021-05-23 15:52:37 +0200

    Delete source cpp directory

--- source cpp/Mine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Bloc.h"
-#include "sha256.h"
-#include "consensus.h"
-#include "params.h"
-
-void Mine(unsigned char * puKey, uint32_t max_purishing, uint32_t utxop, char * fpathbuffer);
-unsigned char * GetTransactionsForNewBlock();
-void IncrementUnofficialBlockFileHeader(uint32_t oBnum, unsigned char * fdata);
